47869bf3c068c5f5340ed51636ab421898b4f44f
roshan-baka-dev <roshanlakra19122000@gmail.com>
2025-08-21 14:57:40 +0530

    added sign-in and sign-up feature

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,14 +4,7 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
+import { Form } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -19,6 +12,12 @@ import { toast } from 'sonner';
 
 import FormField from './FormField';
 import { useRouter } from 'next/navigation';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '@/firebase/client';
+import { signIn, signUp } from '@/lib/actions/auth.action';
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -42,12 +41,51 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(data: z.infer<typeof formSchema>) {
     try {
       if (type === 'sign-up') {
+        const { name, email, password } = data;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
         toast.success('Account created successfully!');
         router.push('/sign-in');
       } else {
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error('Sign in Failed. Please try again.');
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
         toast.success('Sign in successfully!');
         router.push('/');
       }
---
+++ firebase/client.ts
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+
+  //   apiKey: 'AIzaSyCUVBuo4R29HRWHLZkn5FprRBETybO50Jc',
+  //   authDomain: 'prepwise-d8d78.firebaseapp.com',
+  //   projectId: 'prepwise-d8d78',
+  //   storageBucket: 'prepwise-d8d78.firebasestorage.app',
+  //   messagingSenderId: '241965686220',
+  //   appId: '1:241965686220:web:b244a5ed4609088aaa5169',
+  //   measurementId: 'G-FMSSH20QMW',
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
